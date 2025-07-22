bd7a7aee95ad0a5df90580c90712a37db7225593
Shresth-001 <shresth.hp@gmail.com>
2025-07-22 11:34:48 +0530

    New Feature

--- New.js
+++ New.js
@@ -1,13 +1,22 @@
 for (let i = 1; i < 100; i++) {
-  print("shresth");
+  console.log("shresth");
 }
 for (let j = 0; j < 10; j++) {
-  print("Kumar");
+  console.log("Kumar");
 }
-var json = require("./testIgnore/config.json");
-console.log(json.name);
-console.log(json.password);
+// var json = require("./testIgnore/config.json");
+// console.log(json.name);
+// console.log(json.password);
 for (let i=0;i<100;i++){
-  print("Hello world")
+  console.log("Hello world")
 }
+
+const str="1234"
+const leng=str.length;
+let vari=""
+for(let i=leng-1;i>=0;i--){
+      vari=vari+str[i]
+}
+console.log(vari);
+
 
